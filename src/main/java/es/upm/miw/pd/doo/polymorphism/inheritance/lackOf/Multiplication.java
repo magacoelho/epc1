1a2a2c476b93a68ab37ad1358d5e775dcfdc5537
magacoelho <mgacoelho@yahoo.es>
2014-10-12 20:41:44 +0200

    Incorporación de clase Multiplication

---
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/inheritance/lackOf/Multiplication.java
@@ -0,0 +1,18 @@
+package es.upm.miw.pd.doo.polymorphism.inheritance.lackOf;
+
+public class Multiplication extends Operation {
+    public Multiplication(final int operator1, final int operator2) {
+        super(operator1, operator2);
+        this.sign= "*";
+    }
+
+    private int sum() {
+        return this.operator1 + this.operator2;
+    }
+
+	@Override
+	public int operar() {
+		return sum();
+	}
+
+}
