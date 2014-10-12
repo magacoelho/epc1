dc1227280b1c18538d210af749894aeddcae0767
magacoelho <mgacoelho@yahoo.es>
2014-10-12 20:27:35 +0200

    Refactorizada Susbtraction heereda de Operation Implementacion de metodo abstracto operar

--- src/main/java/es/upm/miw/pd/doo/polymorphism/inheritance/lackOf/Subtraction.java
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/inheritance/lackOf/Subtraction.java
@@ -1,22 +1,14 @@
 package es.upm.miw.pd.doo.polymorphism.inheritance.lackOf;
 
-public class Subtraction {
-    private final int operator1, operator2;
-
+public class Subtraction extends Operation {
+  
     public Subtraction(final int operator1, final int operator2) {
-        this.operator1 = operator1;
-        this.operator2 = operator2;
+       super(operator1, operator2);
+       this.sign="-";
+        
     }
 
-    public int getOperator1() {
-        return operator1;
-    }
-
-    public int getOperator2() {
-        return operator2;
-    }
-
-    public int subtract() {
+     public int subtract() {
         return this.operator1 - this.operator2;
     }
 
@@ -24,5 +16,11 @@ public class Subtraction {
     public String toString() {
         return "[" + operator1 + "-" + operator2 + "]";
     }
+
+	@Override
+	public int operar() {
+		
+		return subtract();
+	}
 
 }
