926c9aa92b33f719b04b3dfc17d08c06e68291b4
magacoelho <mgacoelho@yahoo.es>
2014-10-12 20:09:54 +0200

    Agregar superClase Operation Refactorizar métodos, agregar extends Addition Reforma de constructores

--- src/main/java/es/upm/miw/pd/doo/polymorphism/inheritance/lackOf/Addition.java
+++ src/main/java/es/upm/miw/pd/doo/polymorphism/inheritance/lackOf/Addition.java
@@ -1,28 +1,13 @@
 package es.upm.miw.pd.doo.polymorphism.inheritance.lackOf;
 
-public class Addition {
-    private final int operator1, operator2;
-
+public class Addition extends Operation {
     public Addition(final int operator1, final int operator2) {
-        this.operator1 = operator1;
-        this.operator2 = operator2;
-    }
-
-    public int getOperator1() {
-        return operator1;
-    }
-
-    public int getOperator2() {
-        return operator2;
+        super(operator1, operator2);
+        this.sign= "+";
     }
 
     public int sum() {
         return this.operator1 + this.operator2;
-    }
-
-    @Override
-    public String toString() {
-        return "[" + operator1 + "+" + operator2 + "]";
     }
 
 }
