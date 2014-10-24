8bec0ebb163e839b48293da5faa3e0ed5820fc5a
1 <1@PC-PROFESOR>
2014-10-24 14:05:56 +0200

    sin terminar

--- src/main/java/es/upm/miw/concesionario/Main.java
+++ src/main/java/es/upm/miw/concesionario/Main.java
@@ -1,10 +1,8 @@
 package es.upm.miw.concesionario;
 
+import upm.jbb.IO;
+
 public class Main {
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
-    }
+   
 
 }
