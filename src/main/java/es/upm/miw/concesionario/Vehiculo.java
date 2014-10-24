7ec695f94d11ca12cce1a0b39f3e5700d31f9213
1 <1@PC-PROFESOR>
2014-10-24 12:15:02 +0200

    Implementacion clase Vehiculo

--- src/main/java/es/upm/miw/concesionario/Vehiculo.java
+++ src/main/java/es/upm/miw/concesionario/Vehiculo.java
@@ -1,10 +1,29 @@
 package es.upm.miw.concesionario;
 
 public class Vehiculo {
-
+   private Integer id;
+   private  String descripcion;
+   
+    public Vehiculo() {
+    super();
+}
+    public Vehiculo(Integer id, String descripcion) {
+    super();
+    this.id = id;
+    this.descripcion = descripcion;
+}
     public Integer getId() {
-        // TODO Auto-generated method stub
-        return null;
+          return this.id;
+     }
+    public String getDescripcion() {
+        return descripcion;
     }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+   
 
 }
