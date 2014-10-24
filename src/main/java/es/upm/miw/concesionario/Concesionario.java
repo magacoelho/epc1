68d20fc67ae365ee2d2d1f2a9f79ffdc38a3a069
1 <1@PC-PROFESOR>
2014-10-24 11:53:40 +0200

    Interfaz publica de Concesionario. Add Clase Vacia Vehiculo

--- src/main/java/es/upm/miw/concesionario/Concesionario.java
+++ src/main/java/es/upm/miw/concesionario/Concesionario.java
@@ -2,4 +2,14 @@ package es.upm.miw.concesionario;
 
 public class Concesionario {
 
+     public void darAlta(Vehiculo v){
+         
+     }
+     public  String todosVehiculos(){
+         return null;
+     }
+     public double determinarAlquiler(int id, int dias){
+         
+         return 0.0;
+     }
 }
