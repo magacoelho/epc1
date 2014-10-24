777418000492d67139e2c68d4f0afc62b01de5cf
1 <1@PC-PROFESOR>
2014-10-24 13:20:01 +0200

    implementar determinarAlquiler ->Coche

--- src/main/java/es/upm/miw/concesionario/Coche.java
+++ src/main/java/es/upm/miw/concesionario/Coche.java
@@ -28,8 +28,29 @@ public class Coche extends Vehiculo{
 
     @Override
     public double determinarAlquiler(int dias) {
-        // TODO Auto-generated method stub
-        return 0;
+        double precioBase=0.0;
+        double valor=0.0;
+        switch(this.categoria){
+        case 'A': precioBase=10;
+                  break;
+        case 'B': precioBase=15;
+                  break;
+        case 'C': precioBase=20;          
+        
+        }
+        if(dias<=3){
+             valor = precioBase*dias;
+             }
+        else{
+             if (dias<=7){
+                  valor = precioBase*3+precioBase*0.8 *(dias-3);
+             }
+             else{
+                 valor = precioBase*3+precioBase*4+precioBase*(dias-7);
+             }
+        }
+        
+        return valor;
     }
 
    
